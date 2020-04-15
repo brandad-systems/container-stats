338995161b663c4d81949db78d4dae8b0bcaadc2
MCOfficer <mcofficer@gmx.de>
2020-04-15 14:45:13 +0200

    Move everything out of main. How did it get there?

--- src/main.rs
+++ src/main.rs
@@ -93,123 +93,123 @@ fn main() {
     match docker.list_containers(None, None, None, ContainerFilters::new()) {
         Ok(result) => handle_containers(&opt, docker, result),
         Err(e) => println!("Error connecting to docker daemon: {}", e),
-    };
+    }
+}
 
-    fn handle_containers(opt: &Opt, docker: Docker, containers: Vec<Container>) {
-        let mut all_stats = gather_stats(opt, docker, containers);
+fn handle_containers(opt: &Opt, docker: Docker, containers: Vec<Container>) {
+    let mut all_stats = gather_stats(opt, docker, containers);
 
-        if let Some(regex) = &opt.regex {
-            all_stats = filter(all_stats, regex);
-        }
+    if let Some(regex) = &opt.regex {
+        all_stats = filter(all_stats, regex);
+    }
 
-        if opt.total {
-            let total = fold(&all_stats, 0, |i, stats| i + stats.memory.0.as_u64());
-            println!("Total: {} ({} B)", ByteSize::b(total), total);
-            return;
-        }
+    if opt.total {
+        let total = fold(&all_stats, 0, |i, stats| i + stats.memory.0.as_u64());
+        println!("Total: {} ({} B)", ByteSize::b(total), total);
+        return;
+    }
 
-        if opt.group_by_prefix || opt.group_by_suffix {
-            let mut grouped_stats = Vec::<ContainerGroup>::new();
-            for stat in &all_stats {
-                let mut split_name = stat.name.split(opt.delimiter);
-                let fix = if opt.group_by_prefix {
-                    split_name.next().unwrap_or(&stat.name).to_string()
-                } else {
-                    split_name.last().unwrap_or(&stat.name).to_string()
-                };
+    if opt.group_by_prefix || opt.group_by_suffix {
+        let mut grouped_stats = Vec::<ContainerGroup>::new();
+        for stat in &all_stats {
+            let mut split_name = stat.name.split(opt.delimiter);
+            let fix = if opt.group_by_prefix {
+                split_name.next().unwrap_or(&stat.name).to_string()
+            } else {
+                split_name.last().unwrap_or(&stat.name).to_string()
+            };
 
-                let mut found = false;
-                for group in &mut grouped_stats {
-                    if group.fix.eq(&fix) {
-                        group.memory = SerializableByteSize(group.memory.0 + stat.memory.0);
-                        group.containers += 1;
-                        found = true;
-                    }
-                }
-
-                if !found {
-                    grouped_stats.push(ContainerGroup {
-                        memory: stat.memory,
-                        containers: 1,
-                        fix: String::from(&fix),
-                    });
+            let mut found = false;
+            for group in &mut grouped_stats {
+                if group.fix.eq(&fix) {
+                    group.memory = SerializableByteSize(group.memory.0 + stat.memory.0);
+                    group.containers += 1;
+                    found = true;
                 }
             }
 
-            if opt.sort {
-                grouped_stats.sort_by(|a, b| b.memory.0.cmp(&a.memory.0));
+            if !found {
+                grouped_stats.push(ContainerGroup {
+                    memory: stat.memory,
+                    containers: 1,
+                    fix: String::from(&fix),
+                });
             }
-            print(opt, &grouped_stats);
-            return;
         }
 
         if opt.sort {
-            all_stats.sort_by(|a, b| b.memory.0.cmp(&a.memory.0));
+            grouped_stats.sort_by(|a, b| b.memory.0.cmp(&a.memory.0));
         }
-        print(opt, &all_stats);
+        print(opt, &grouped_stats);
+        return;
     }
 
-    fn gather_stats(opt: &Opt, docker: Docker, containers: Vec<Container>) -> Vec<ContainerStats> {
-        let mut all_stats = Vec::new();
-        for container in containers {
-            let mut memory = 0;
-            let mut pids = Vec::<i64>::new();
-
-            if opt.top {
-                let processes = docker.processes(&container.Id).unwrap();
-                for p in processes {
-                    pids.push(p.pid.parse::<i64>().unwrap());
-                }
-            } else {
-                pids.push(docker.container_info(&container.Id).unwrap().State.Pid);
-            }
-
-            for pid in pids {
-                memory += get_process_memory_bytes(opt, pid) as u64;
-            }
-
-            all_stats.push(ContainerStats {
-                id: container.Id,
-                name: join(container.Names, ", "),
-                memory: SerializableByteSize(ByteSize::b(memory)),
-            })
-        }
-        all_stats
+    if opt.sort {
+        all_stats.sort_by(|a, b| b.memory.0.cmp(&a.memory.0));
     }
+    print(opt, &all_stats);
+}
 
-    fn get_process_memory_bytes(opt: &Opt, pid: i64) -> usize {
+fn gather_stats(opt: &Opt, docker: Docker, containers: Vec<Container>) -> Vec<ContainerStats> {
+    let mut all_stats = Vec::new();
+    for container in containers {
         let mut memory = 0;
+        let mut pids = Vec::<i64>::new();
 
-        if opt.memory_backend.eq("procmaps") {
-            let maps = get_process_maps(pid as Pid).unwrap();
-            for map in maps {
-                memory += map.size();
+        if opt.top {
+            let processes = docker.processes(&container.Id).unwrap();
+            for p in processes {
+                pids.push(p.pid.parse::<i64>().unwrap());
             }
         } else {
-            let p = Process::new(pid as i32).expect("Failed to access process");
-            let stat = p.stat().expect("Failed to get process stat");
-            memory = match opt.memory_backend.as_ref() {
-                "rss" => stat.rss_bytes() as usize,
-                "vsz" => stat.vsize as usize,
-                &_ => panic!("Got unknown memory backend"),
-            }
+            pids.push(docker.container_info(&container.Id).unwrap().State.Pid);
         }
-        memory
-    }
 
-    fn filter(stats: Vec<ContainerStats>, pattern: &str) -> Vec<ContainerStats> {
-        let re = Regex::new(pattern).expect("Failed to create Regex pattern!");
-        stats
-            .into_iter()
-            .filter(|s| re.is_match(&s.name).unwrap())
-            .collect()
-    }
-
-    fn print(opt: &Opt, to_print: &[impl Tabled + Serialize]) {
-        if opt.json {
-            println!("{}", serde_json::to_string_pretty(to_print).unwrap())
-        } else {
-            println!("{}", table(to_print));
+        for pid in pids {
+            memory += get_process_memory_bytes(opt, pid) as u64;
         }
+
+        all_stats.push(ContainerStats {
+            id: container.Id,
+            name: join(container.Names, ", "),
+            memory: SerializableByteSize(ByteSize::b(memory)),
+        })
+    }
+    all_stats
+}
+
+fn get_process_memory_bytes(opt: &Opt, pid: i64) -> usize {
+    let mut memory = 0;
+
+    if opt.memory_backend.eq("procmaps") {
+        let maps = get_process_maps(pid as Pid).unwrap();
+        for map in maps {
+            memory += map.size();
+        }
+    } else {
+        let p = Process::new(pid as i32).expect("Failed to access process");
+        let stat = p.stat().expect("Failed to get process stat");
+        memory = match opt.memory_backend.as_ref() {
+            "rss" => stat.rss_bytes() as usize,
+            "vsz" => stat.vsize as usize,
+            &_ => panic!("Got unknown memory backend"),
+        }
+    }
+    memory
+}
+
+fn filter(stats: Vec<ContainerStats>, pattern: &str) -> Vec<ContainerStats> {
+    let re = Regex::new(pattern).expect("Failed to create Regex pattern!");
+    stats
+        .into_iter()
+        .filter(|s| re.is_match(&s.name).unwrap())
+        .collect()
+}
+
+fn print(opt: &Opt, to_print: &[impl Tabled + Serialize]) {
+    if opt.json {
+        println!("{}", serde_json::to_string_pretty(to_print).unwrap())
+    } else {
+        println!("{}", table(to_print));
     }
 }
